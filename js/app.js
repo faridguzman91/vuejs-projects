d414af967b2ab4e48350c371d70f4026aa999696
Jimmy Guzman <jimmy@creapolis.com>
2021-08-15 20:26:19 +0200

    image

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue',
         product: 'Socks',
         description: 'These are cheap wool socks',
-        image: './img/1200x1071.jpg',
+        selectedVariant: 0,
         link: 'https://www.bol.com/nl/nl/p/18-paar-pierre-cardin-herensokken-met-hoog-gehalte-aan-katoen-zwart-43-46/9300000025356074/?Referrer=ADVNLGOO002053-G-119117156342-S-1248283322046-9300000025356074&gclid=CjwKCAjw092IBhAwEiwAxR1lRicKZgioDI1CVKq2Ic_57KXZemlHPzgj8FJimJKXYR7m1iOv9DBlFBoCiAYQAvD_BwE',
         inStock: true,
         inventory: 0,
@@ -27,13 +28,21 @@ var app = new Vue({
         addToCart: function() {
             this.cart += 1
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
         },
         removeFromCart: function() {
             this.cart -= 1
         }
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        }
+    }
 
 
 
