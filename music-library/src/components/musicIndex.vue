462d86ce946c0122df24f808402b3b702dc10aca
Jimmy Guzman <jimmy@creapolis.com>
2021-08-16 21:40:46 +0200

    json and index

---
+++ music-library/src/components/musicIndex.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table is-fullwidth is-striped is-hoverable is-narrow">
+    <thead>
+      <th>Artist</th>
+      <th>Title</th>
+      <th>Album</th>
+      <th>Genre</th>
+    </thead>
+
+    <tbody v-for="(song, index) in songs" v-bind:key="index">
+      <tr>
+        <td>{{ song.artist }}</td>
+        <td>{{ song.title }}</td>
+        <td>{{ song.album }}</td>
+        <td>{{ song.genre }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import musicData from "@/assets/list.json";
+export default {
+  data() {
+    return {
+      songs: musicData,
+    };
+  },
+};
+</script>
+
+<style></style>
